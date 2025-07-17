12ac0be7320415552b657888bde577a12ad82108
Thatt <tom.tom.thanet@gmail.com>
2025-07-17 14:35:05 +0700

    Fix price_ratio function to handle zero values gracefully and update test cases

--- src/app/balance_price_calulation.py
+++ src/app/balance_price_calulation.py
@@ -25,7 +25,8 @@ def price_ratio(p_dict: dict) -> dict:
 
     for key, value in p_dict.items():
         if value == 0:
-            raise ValueError(f"Price for {key} cannot be zero.")
+            print(f"Price for {key} cannot be zero.")
+            continue
         sum += value
 
     ration = 100 / sum
@@ -33,12 +34,24 @@ def price_ratio(p_dict: dict) -> dict:
     result_dict = {}
 
     for key, value in p_dict.items():
-        result_dict[key] = value * ration
+        try:
+            result_dict[key] = value * ration
+        except Exception as e:
+            print(f"Error processing {key}: {e}")
+            continue
 
     return result_dict
 
 
-test_dict = {"item1": 50, "item2": 30, "item3": 20}
+test_dict = {"item1": 5, "item2": 3, "item3": 2}
+test_dict2 = {"item1": 10, "item2": 0, "item3": 5}
+test_dict3 = {"item1": 35, "item2": 14, "item3": 9}
+test_dict4 = {"item1": 150, "item2": 780, "item3": 456}
+
 
 if __name__ == "__main__":
     print(price_ratio(test_dict))
+    print(price_ratio(test_dict2))
+
+    print(price_ratio(test_dict3))
+    print(price_ratio(test_dict4))
