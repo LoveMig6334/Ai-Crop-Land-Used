526e4ff653b60a7a2515486549a7207f1e09b023
Thatt <tom.tom.thanet@gmail.com>
2025-10-04 15:18:18 +0700

    Add CUDA availability check and related functionality

--- machine_pre.py
+++ machine_pre.py
@@ -1,3 +1,6 @@
+from torch import cuda
+
+
 def main() -> None:
     epsilon = 1
 
@@ -12,5 +15,16 @@ def main() -> None:
         print("Epsilon is still significant.")
 
 
+def cuda_test() -> None:
+    if cuda.is_available():
+        print("CUDA is available.")
+        print(f"CUDA Device Count: {cuda.device_count()}")
+        print(f"Current CUDA Device: {cuda.current_device()}")
+        print(f"CUDA Device Name: {cuda.get_device_name(cuda.current_device())}")
+    else:
+        print("CUDA is not available.")
+
+
 if __name__ == "__main__":
     main()
+    cuda_test()
